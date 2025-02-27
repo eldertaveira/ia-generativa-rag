b04d6ef6d5cf9e83aae9b265518e2b9b398db629
eldertaveira <elder.samuel95@gmail.com>
2025-02-27 20:05:29 -0300

    Corrigindo dependências e integração com LangChain Google GenAI

--- utils/chatbot.py
+++ utils/chatbot.py
@@ -1,7 +1,8 @@
 from dotenv import load_dotenv
 from langchain_community.vectorstores import FAISS
 from langchain.embeddings.base import Embeddings
-import google.generativeai as genai
+# import google.generativeai as genai
+from langchain_google_genai import GoogleGenerativeAI
 import os
 
 
@@ -18,45 +19,53 @@ google_credentials = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
 
 # Verificar se a chave foi carregada corretamente
 if not api_key:
-    raise ValueError("A chave da API do Gemini não foi encontrada. Verifique o arquivo .env.")
+    raise ValueError(
+        "A chave da API do Gemini não foi encontrada. Verifique o arquivo .env.")
 if not google_credentials:
-    raise ValueError("As credenciais do Google não foram encontradas. Verifique a variável GOOGLE_APPLICATION_CREDENTIALS no arquivo .env.")
+    raise ValueError(
+        "As credenciais do Google não foram encontradas. Verifique a variável GOOGLE_APPLICATION_CREDENTIALS no arquivo .env.")
 
 # Verificar se o arquivo JSON de credenciais do Google existe
 if not os.path.exists(google_credentials):
-    raise ValueError(f"O arquivo de credenciais do Google não foi encontrado no caminho especificado: {google_credentials}")
+    raise ValueError(
+        f"O arquivo de credenciais do Google não foi encontrado no caminho especificado: {google_credentials}")
 
-# configuração da chave API (api_key) para consumir modelos como o Gemini 1.5 (da Google DeepMind). 
-genai.configure(api_key=api_key)
+# configuração da chave API (api_key) para consumir modelos como o Gemini 1.5 (da Google DeepMind).
+# genai.configure(api_key=api_key)
+llm = GoogleGenerativeAI(model="gemini-1.5", google_api_key=api_key)
 
 
 def create_vectorstore(chunks):
     # Inicializa o modelo de embeddings Gemini
     embeddings = GoogleGenerativeAIEmbeddings(
-        model="models/embedding-001", # Esse modelo é otimizado para gerar embeddings semânticos.
+        # Esse modelo é otimizado para gerar embeddings semânticos.
+        model="models/embedding-001",
         task_type="retrieval_document",
-    )  
+        google_api_key=api_key  # linha adicionada 27/02
+    )
 
     # GoogleGenerativeAIEmbeddings → Usa o modelo Gemini para gerar embeddings.
     # FAISS → Cria o banco vetorial para fazer busca semântica.
     # from_texts → Converte os textos em vetores e armazena no FAISS.
-    vectorstore = FAISS.from_texts(texts=chunks, embedding=embeddings) # Cria o banco vetorial para fazer busca semântica.
+    # Cria o banco vetorial para fazer busca semântica.
+    vectorstore = FAISS.from_texts(texts=chunks, embedding=embeddings)
     print("Vectorstore criado com sucesso!")
-    
-    return vectorstore
 
+    return vectorstore
 
 
 def create_conversation_chain(vectorstore):
     model_Gemini = ChatGoogleGenerativeAI(
-        model= "gemini-2.0-flash",
+        model="gemini-2.0-flash",
         temperature=0.2,
+        google_api_key=api_key  # linha adicionada 27/02
     )
-    memory = ConversationBufferMemory(memory_key='chat_history', return_messages= True)
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
 
     conversation_chain = ConversationalRetrievalChain.from_llm(
         model_Gemini,
-        retriever = vectorstore.as_retriever(),
-        memory = memory
+        retriever=vectorstore.as_retriever(),
+        memory=memory
     )
     return conversation_chain
